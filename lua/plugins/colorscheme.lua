59243db3185d1868b2e7f46eb277b07006019750
Tu Nguyen <minhtu1392000@gmail.com>
2024-06-28 08:21:47 +0700

    updated via bak at Fri Jun 28 08:21:47 +07 2024

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,18 +1,8 @@
 return {
   {
-    "olimorris/onedarkpro.nvim",
-    priority = 1000,
-    opts = {
-      colors = {
-        onedark = { bg = "#1E2227" },
-      },
-    },
-  },
-
-  {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "onedark",
+      colorscheme = "catppuccin-mocha",
     },
   },
 }
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -3,4 +3,17 @@ return {
     "neoclide/coc.nvim",
     branch = "release",
   },
+  {
+    "hrsh7th/nvim-cmp",
+    init = function()
+      local cmp = require("cmp")
+      -- `/` cmdline setup.
+      cmp.setup.cmdline("/", {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = "buffer" },
+        },
+      })
+    end,
+  },
 }
